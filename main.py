129649d23e929369b6d0ec37ba240faab77c626b
Ayub <ayub.kosimov@gmail.com>
2024-02-17 14:53:12 +0300

    wip

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from parse_catalog import get_json_response
 SCRAPE_ME_URL = 'https://scrapeme.live/shop/'
 
 if __name__ == '__main__':
-    print(get_json_response(SCRAPE_ME_URL, True))  # Configure call
+    print(get_json_response(url=SCRAPE_ME_URL, parse_individual_page=True))  # Configure call
